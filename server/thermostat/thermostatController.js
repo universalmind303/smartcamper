c919757eab09ab7cf5dabbd8b9b6c8dc61e3331e
universalmind303 <universalmind.candy@gmail.com>
2018-05-12 18:32:29 -0500

    added docker support

--- server/thermostat/thermostatController.js
+++ server/thermostat/thermostatController.js
@@ -8,18 +8,17 @@ import {
 const router = Router()
 
 router.get('/farenheit', async (req, res) => {
-  const temperature = await readTempsFahrenheit()
-  res.json(temperature)
+  // const temperature = await readTempsFahrenheit()
+  res.json('temperature')
 })
 
 router.get('/celcius', async (req, res) => {
-  const temperature = await readTempsCelcius()
-  res.json(temperature)
+  // const temperature = await readTempsCelcius()
+  res.json('temperature')
 })
 
 router.post('', async (req, res) => {
   const { temperature, mode } = req.body
-  console.log(temperature, mode)
   await setTemperature(temperature)
   res.json(temperature)
 })
